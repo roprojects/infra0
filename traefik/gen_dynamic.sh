fcd060eee72fe6461ddc8cb2f840f83333b82ec2
Rafal Opiela <work.opiela@gmail.com>
2024-10-28 22:32:06 +0000

    Updating configuration files

--- traefik/gen_dynamic.sh
+++ traefik/gen_dynamic.sh
@@ -6,13 +6,15 @@ cat config/dynamic.yaml.template > config/dynamic.yaml
 
 D="homelab.lan"
 
-for i in $(ls certs/*.crt | grep -v "${D}")
+cd certs
+
+for i in $(ls *.crt | grep -v "${D}")
 do
  A=$(echo ${i} | sed -r -e 's/\.crt//g')
 
- ln -sf ${i} ${A}.${D}.crt
- ln -sf ${A}.key ${A}.${D}.key
+ ln -f ${i} ${A}.${D}.crt
+ ln -f ${A}.key ${A}.${D}.key
 
- echo "      - certFile: /${A}.${D}.crt" >> config/dynamic.yaml
- echo "        keyFile: /${A}.${D}.key" >> config/dynamic.yaml
+ echo "      - certFile: /certs/${A}.${D}.crt" >> ../config/dynamic.yaml
+ echo "        keyFile: /certs/${A}.${D}.key" >> ../config/dynamic.yaml
 done
